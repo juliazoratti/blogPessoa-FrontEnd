e71516d105b3f13ff3c94c445596250cf0b665f7
julia <juliazoratti@gmail.com>
2021-04-30 11:41:48 -0300

    implementando o consumo de API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'}, /*quando o path for vazio ele vai redirecionar para a página entrar*/
   {path: 'entrar', component: EntrarComponent}, /*quando o path for entrar ele vai para a página de login*/
-  {path: 'cadastrar', component: CadastrarComponent} /*quando o path for cadastrar ele vai pra página de cadastro*/
+  {path: 'cadastrar', component: CadastrarComponent}, /*quando o path for cadastrar ele vai pra página de cadastro*/
+  {path: 'inicio', component: InicioComponent}
 ];
 
 @NgModule({

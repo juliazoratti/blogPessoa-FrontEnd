9c1a05acb4e455009c4b739a8b05fae6543d64bc
julia <juliazoratti@gmail.com>
2021-05-03 15:16:52 -0300

    Implementação do componente tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'}, /*quando o path for vazio ele vai redirecionar para a página entrar*/
   {path: 'entrar', component: EntrarComponent}, /*quando o path for entrar ele vai para a página de login*/
   {path: 'cadastrar', component: CadastrarComponent}, /*quando o path for cadastrar ele vai pra página de cadastro*/
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
